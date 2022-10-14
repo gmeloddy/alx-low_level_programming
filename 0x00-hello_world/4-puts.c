fb8e8ece3c1989030a480dfab8b90f272a88e503
gmeloddy <gm.gmelody@gmail.com>
2022-10-13 22:20:45 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
  *main- has a return type int
- *Return: By default returns zero if no erros otherwise returns non-zero value.
-      */
+ *Return: By default returns zero if no erros in your code       */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - has a return type which is int
+ * Return: By default returns zero if no erros in your code
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art\n");
+
+return (0);
+}
+
