ab7a32005fd782fca4e4b72e2007a58c95413e46
gmeloddy <gm.gmelody@gmail.com>
2022-10-15 12:32:51 -0700

    ini

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - return integer value by defult
+*Return: zero
+*/
+
+int main(void)
+{
+char letter1;
+char letter2;
+
+for (letter1 = 'a'; letter1 <= 'z'; letter++)
+	for (letter2 = 'A'; letter2 <= 'Z' letter++)
+	putchar (letter1 letter2);
+putchar ('\n');
+return (0);
+}
