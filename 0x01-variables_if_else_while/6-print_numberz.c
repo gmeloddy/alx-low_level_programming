1ed2af708779eaa4f62a7eb4d071e2bc6613ea87
gmeloddy <gm.gmelody@gmail.com>
2022-10-15 20:54:38 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+n - returns int value                                                                                     
+
+ *Return: zero                                                                                                 
+
+ */                                                                                                                                                                                                                        
+
+ int main (void)                                                                                                                                                                                                             
+
+ {                                                                                                             
+
+ int num = 48;                                                                                                                                                                                                
+
+ while (num <= 57)                                                                                     
+
+ {                                                                                                     
+
+ putchar(num);                                                                                                                                                                                               
+
+ }                                                                                                     
+
+ putchar('')
