affc25f627edb96cc027dc551e51e2c3ec385eed
gmeloddy <gm.gmelody@gmail.com>
2022-10-20 01:59:15 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,17 @@
 *print_alphabet_x10 - prints 10 times the alphabet, in lowercase
 */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
 
 	while (count++ <= 9)
 	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar('\n');
 
-	_putchar(letter);
-	_putchar('\n');
-
+	}
 }
