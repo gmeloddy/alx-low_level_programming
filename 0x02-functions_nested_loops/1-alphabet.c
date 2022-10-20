fc29e5fad0c5c27974099f528c88e63a3a8f73f3
gmeloddy <gm.gmelody@gmail.com>
2022-10-20 00:19:01 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*print_alphabet - prints alphabets in lowercase, followed by newline
+*/
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; leter <= 'z'; letter++)
+	_putchar(letter);
+	_putchar('\n');
+
+}
