eded4a5d05ae36edd4f91db6210639d261525430
gmeloddy <gm.gmelody@gmail.com>
2022-10-22 03:33:55 -0700

    102-print_comb5

--- 102-print_comb5.c
+++ 102-print_comb5.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
-* main - combination of two two digits
-*
-* Return: Always 0.
-*/
-
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
+{
+	int p, q;
+
+	for (p = 0; p <= 98; p++)
 	{
-		int num1, num2;
-
-		for (num1 = 0; num1 <= 98; num1++)
+		for (q = p + 1; q <= 99; q++)
 		{
-			for (num2 = num1 + 1; num2 <= 99; num2++)
-			{
-				putchar((num1 / 10) + '0');
-				putchar((num1 % 10) + '0');
-				putchar(' ');
-				putchar((num2 / 10) + '0');
-				putchar((num2 % 10) + '0');
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((q / 10) + '0');
+			putchar((q % 10) + '0');
 
-				if (num1 == 98 && num2 == 99)
-					continue;
+			if (p == 98 && q == 99)
+				continue;
 
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
-
-		putchar('\n');
-		return (0);
 	}
+	putchar('\n');
+	return (0);
+}
